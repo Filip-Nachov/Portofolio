4fa47c7c8ac54da7a036d078a2fdbf8d37c3d13b
Filip Nachov <fnachov@gmail.com>
2025-03-13 18:39:13 +0100

    made a counter that doesnt work

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-const app = createApp(App)
+const app = createApp({
+    data() {
+        return {
+            count: 0
+        }
+     }
+})
 
 app.mount('#app')
