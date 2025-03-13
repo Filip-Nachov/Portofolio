9a0c1fd785a82d5518f7b1b9dea1526b2a5a11be
Filip Nachov <fnachov@gmail.com>
2025-03-13 19:10:06 +0100

    made a counter and made it work cuz thats just who I am

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+console.log(count)
+
+function increment() {
+  count.value++
+}
+</script>
+
 <template>
+  <button @click="increment">
+    {{ count }}
+  </button>
 </template>
+
+
